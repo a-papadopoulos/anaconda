5d3b4e802bf298ae4185de425e69e08857ac07fe
Alexandre Papadopoulos <32906275+a-papadopoulos@users.noreply.github.com>
2018-03-30 15:11:04 +0200

    Update version of anaconda install script

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,11 +2,11 @@ class anaconda::params {
 
     $base_url = $::domain ? {
       #'atx.continuum.io'=>"http://filer.atx.continuum.io/released/1.5.0",
-      default           =>"http://repo.continuum.io/archive",
+      default           =>"https://repo.continuum.io/archive/",
     }
 
     $installer = $::kernel ? {
-      /(L|l)inux/ => "Anaconda3-4.4.0-Linux-x86_64.sh",
+      /(L|l)inux/ => "Anaconda3-5.1.0-Linux-x86_64.sh",
       'windows'   => "Anaconda-4.4.0-Windows-x86_64.exe",
       'Darwin'    => "Anaconda3-4.4.0-MacOSX-x86_64.sh",
     }
